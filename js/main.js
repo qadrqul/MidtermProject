29532344044056d7ddcdcd66cab3fae742b60d3e
JagdpantherK <jagpanthertitanium@gmail.com>
2022-11-03 07:21:31 +0600

    Add Quotes section Change .png files to .webp Make optimization and validation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
+const menu = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const gojosQuote = document.querySelector('.quote-1');
+const button = document.querySelector('#quote-button');
+let clicks = 0;
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
+fetch("https://animechan.vercel.app/api/random/character?name=saitama")
+    .then(response => response.json())
+    .then(quote => {
+        gojosQuote.innerHTML=quote.quote;
+    });
+
+button.addEventListener("click", (where, element) => {
+    clicks += 1;
+    let color = "#";
+    color += Math.random().toString(16).slice(2,8);
+    button.style.background = color;
+    fetch("https://animechan.vercel.app/api/random/character?name=saitama")
+        .then(response => response.json())
+        .then(quote => {
+            gojosQuote.innerHTML=quote.quote;
+        });
+})
