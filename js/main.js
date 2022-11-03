2c5650187c210027f3487332b6c947367dd85d21
JagdpantherK <jagpanthertitanium@gmail.com>
2022-11-03 07:42:12 +0600

    Surprise

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ const menu = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 const gojosQuote = document.querySelector('.quote-1');
 const button = document.querySelector('#quote-button');
+const image = document.querySelector('.quote-img')
 let clicks = 0;
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
+image.innerHTML = `<img src="../images/Satoru_Gojo.webp" alt="photo"/>`;
 fetch("https://animechan.vercel.app/api/random/character?name=saitama")
     .then(response => response.json())
     .then(quote => {
@@ -23,4 +25,10 @@ button.addEventListener("click", (where, element) => {
         .then(quote => {
             gojosQuote.innerHTML=quote.quote;
         });
+    if(clicks===5 || clicks===10 || clicks===50 || clicks===100){
+        image.innerHTML = `<img src="../images/Agay.webp" alt="photo"/>`;
+    }
+    else {
+        image.innerHTML = `<img src="../images/Satoru_Gojo.webp" alt="photo"/>`;
+    }
 })
